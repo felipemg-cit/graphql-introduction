1149dcb57e904549969bd6f7e9e78cc0fa8fa92c
Felipe Matheus Guimaraes dos Santos <felipemg@ciandt.com>
2021-04-04 23:48:42 -0300

    [+]Adds some error handlings

--- src/main/java/com/learngraphq/resolver/bank/ClientResolver.java
+++ src/main/java/com/learngraphq/resolver/bank/ClientResolver.java
@@ -3,6 +3,9 @@ package com.learngraphq.resolver.bank;
 import com.learngraphq.domain.bank.BankAccount;
 import com.learngraphq.domain.bank.Client;
 import com.learngraphq.domain.bank.Currency;
+import graphql.GraphQLException;
+import graphql.execution.DataFetcherResult;
+import graphql.kickstart.execution.error.GenericGraphQLError;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import graphql.kickstart.tools.GraphQLResolver;
 import lombok.extern.slf4j.Slf4j;
@@ -14,12 +17,18 @@ import java.util.UUID;
 @Component
 public class ClientResolver implements GraphQLResolver<BankAccount> {
 
-    public Client client(BankAccount bankAccount){
+    public DataFetcherResult<Client> client(BankAccount bankAccount){
         log.info("Requesting client data for bank id {}", bankAccount.getId());
-        return Client.builder()
-                .id(UUID.randomUUID())
-                .firstName("Wanilton")
-                .lastName("Filho")
+//        throw new GraphQLException("Client unavailable");
+//        throw new RuntimeException("Client unavailable");
+        //Return just a part of the information
+        return DataFetcherResult.<Client>newResult()
+                .data(Client.builder()
+                    .id(UUID.randomUUID())
+                    .firstName("Wanilton")
+                    .lastName("Filho")
+                    .build())
+                .error(new GenericGraphQLError("Could not get sub-client id"))
                 .build();
     }
 
